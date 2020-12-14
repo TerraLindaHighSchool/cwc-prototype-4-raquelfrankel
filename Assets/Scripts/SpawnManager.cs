0a668aa2c7c5cd5785d021a5080c301337bc93dd
raquelfrankel <60672345+raquelfrankel@users.noreply.github.com>
2020-12-14 12:25:31 -0800

    4.4.3 complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,7 +9,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave();
+        SpawnEnemyWave(3);
 
     }
 
@@ -19,9 +19,9 @@ public class SpawnManager : MonoBehaviour
         
     }
 
-    void SpawnEnemyWave()
+    void SpawnEnemyWave(int enemiesToSpawn)
     {
-        for(int i = 0; i < 3; i = i++)
+        for(int i = 0; i < enemiesToSpawn; i = i++)
         {
 
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
